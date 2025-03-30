41b61b552f5f7695a5950120e9bec9c65dc7c350
MIN <mhroh01@naver.com>
2025-03-30 14:17:34 +0900

    init

--- src/data/data_loader_teacher.py
+++ src/data/data_loader_teacher.py
@@ -4,62 +4,23 @@ from PIL import Image
 import imageio.v2 as imageio
 from torchvision import transforms
 from dotenv import load_dotenv
-from torch.utils.data import TensorDataset, DataLoader
+from torch.utils.data import DataLoader
+from load_data import get_data_list, customDataset
+import yaml
 
-# 1. env로부터 경로 가져오기
+# env로부터 경로 가져오고, 인풋이미지와 gt 이미지 경로만 로드
 load_dotenv(dotenv_path="../../.env")
-
 dataset_path = os.getenv("BLENDEDMVS_DATASET_PATH")
 
-input_image_paths = sorted(glob.glob(os.path.join(dataset_path, "*", "blended_images", "*_masked.jpg")))
-gt_image_paths = sorted(glob.glob(os.path.join(dataset_path, "*", "rendered_depth_maps", "*.pfm")))
+x_path_list , gt_path_list= get_data_list(dataset_path)
+dataset = customDataset(x_path_list,gt_path_list,transform=False)
 
-# 변환 객체 생성 (이미지를 448x448로 센터크롭 후, [0,1] 범위의 텐서로 변환)
-to_tensor = transforms.Compose([
-    transforms.CenterCrop(448),
-    transforms.ToTensor()
-])
+config_path = "path/to/config.yaml"
+with open(config_path, "r") as file:
+    config = yaml.safe_load(file)
 
+hyper_params = config["hyper_parameter"]
+batch_size = hyper_params["batch_size"]
 
-# input 이미지: RGB 이미지로 변환 후 텐서화
-for path in input_image_paths:
-    try:
-        img = Image.open(path).convert('RGB')
-        tensor_img = to_tensor(img)
-        images.append(tensor_img)
-        #print(f"Loaded image (tensor): {path}")
-    except Exception as e:
-        print(f"Error loading {path}: {e}")
+dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
 
-# gt (depth map): 'F' 모드로 변환하여 텐서화
-for path in gt_image_paths:
-    try:
-        img = Image.open(path).convert('F')
-        tensor_img = to_tensor(img)
-        gt.append(tensor_img)
-        #print(f"Loaded gt (tensor): {path}")
-    except Exception as e:
-        print(f"Error loading {path}: {e}")
-
-print(f"총 {len(images)}개의 이미지, {len(gt)}개의 정답")
-
-
-
-load 
-
-
-MVS_input = torch.stack(images)
-MVS_gt = torch.stack(gt)
-
-
-
-dataset = TensorDataset(MVS_input, MVS_gt)
-dataloader = DataLoader(dataset, batch_size=4, shuffle=True)
-
-
-# DataLoader에서 첫 배치만 꺼내서 확인
-for batch in dataloader:
-    inputs, targets = batch
-    print("Input batch shape:", inputs.shape)
-    print("GT batch shape:", targets.shape)
-    break  # 첫 배치만 확인
--- src/data/load_data.py
+++ src/data/load_data.py
@@ -1,4 +1,5 @@
 ## 여기서 리스트 가져오는 함수를 하나 만들기. 
+
 import os
 import glob
 
@@ -8,4 +9,40 @@ def get_data_list(dataset_path,teacher=True):
         gt_image_paths = sorted(glob.glob(os.path.join(dataset_path, "*", "rendered_depth_maps", "*.pfm")))
     
     else :
-        input_image_paths = sorted(glob.glob(os.path.join(dataset_path, "*", "blended_images", "*_masked.jpg")))
+        input_image_paths = sorted(glob.glob(os.path.join(dataset_path, "*.jpg")))
+        gt_image_paths = sorted(glob.glob(os.path.join(dataset_path, "pseudo_depth_maps", "*.pfm")))    ## teacher model로부터 만들어지는 pseudo map
+    
+    return input_image_paths, gt_image_paths
+
+
+# 커스텀 데이터셋 생성
+class customDataset(torch.utils.data.Dataset):
+    def __init__(self,x_path,gt_path,transform=True):
+        super().__init__()
+        self.x_path = x_path
+        self.gt_path = gt_path
+        self.transform = transform
+        self.basic_transformation = transforms([
+            transforms.CenterCrop(448),
+            transforms.ToTensor()
+        ])
+        #self.jittering_transformation 
+        #self.gaussian_blur_transformation
+
+    def __len__(self):
+        if len(self.x_path) == len(self.gt_path):
+            return len(self.x_path)
+        else assert "x_path와 gt_path의 길이 불일치 !!"
+
+    def __getitem__(self,idx):
+        # 지금까지 해왓던 일반적인 방식과는 다르게, 여기서 데이터를 "직접 로드"하고 리턴하는 식으로 진행
+        x = Image.open(self.x_path[idx]).convert('RGB')
+        gt = Image.open(self.gt_path[idx]).convert('F')
+
+        x = self.basic_transformation(x)
+        gt = self.basic_transformation(gt)
+
+        # if self.transform :
+        # TBD.... for student..
+
+        return x, gt
